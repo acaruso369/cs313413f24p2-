15dfbc1b1d240b484869498598f8008a8b347258
acaruso369 <94490665+acaruso369@users.noreply.github.com>
2024-09-21 21:16:23 -0500

    Homework done and is ready to be graded

--- src/test/java/cs271/lab/list/TestIterator.java
+++ src/test/java/cs271/lab/list/TestIterator.java
@@ -48,17 +48,17 @@ public class TestIterator {
     assertEquals(33, i.next().intValue());
     // TODO fix the expected values in the assertions below
     assertTrue(i.hasNext());
-    assertEquals(0, i.next().intValue());
+    assertEquals(77, i.next().intValue());
     assertTrue(i.hasNext());
-    assertEquals(0, i.next().intValue());
+    assertEquals(44, i.next().intValue());
     assertTrue(i.hasNext());
-    assertEquals(0, i.next().intValue());
+    assertEquals(77, i.next().intValue());
     assertTrue(i.hasNext());
-    assertEquals(0, i.next().intValue());
+    assertEquals(55, i.next().intValue());
     assertTrue(i.hasNext());
-    assertEquals(0, i.next().intValue());
+    assertEquals(77, i.next().intValue());
     assertTrue(i.hasNext());
-    assertEquals(0, i.next().intValue());
+    assertEquals(66, i.next().intValue());
     assertFalse(i.hasNext());
   }
 
@@ -80,7 +80,8 @@ public class TestIterator {
     // TODO using assertEquals and List.of, express which values are left in the list
     // See TestList.java for examples of how to use List.of; also see the Java List
     // interface for more information
-    fail("Not yet implemented"); // remove this line when done
+    assertEquals(List.of(33,44,55,66),list);
+    // fail("Not yet implemented"); // remove this line when done
   }
 
   @Test
@@ -92,8 +93,22 @@ public class TestIterator {
     list.add(55);
     list.add(77);
     list.add(66);
+
     double sum = 0;
     int n = 0;
+
+    Iterator<Integer> iterator = list.iterator();
+
+    while (iterator.hasNext()) {
+      sum += iterator.next(); // Add the next value to sum
+      n++;
+    }
+
+    // Calculate the average
+    if (n > 0) {
+      double average = sum / n;
+    }
+
     // TODO use an iterator and a while loop to compute the average (mean) of the values
     // (defined as the sum of the items divided by the number of items)
     // testNonempty shows how to use an iterator; use i.hasNext() in the while loop condition
